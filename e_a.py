64c9f94854a5ca2f5e97f71ba6aedb292cb7ac9f
victordaniel <victor.offiial@gmail.com>
2023-02-14 10:44:34 +0530

    created a dictionary

--- e_a.py
+++ e_a.py
@@ -1,3 +1,17 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
+import random
+
+# Define the possible values for each key
+countries = ['USA', 'Canada', 'UK', 'Germany']
+ages = [20, 25, 30, 35]
+years = [2000, 2001, 2002, 2003]
+
+# Create a dictionary with random values for each key
+data_dict = {'country': [], 'age': [], 'year': []}
+for i in range(4):
+    data_dict['country'].append(random.choice(countries))
+    data_dict['age'].append(random.choice(ages))
+    data_dict['year'].append(random.choice(years))
+
+# Print the dictionary
+print(data_dict)
+
